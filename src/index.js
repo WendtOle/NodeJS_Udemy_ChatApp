c8f22ac08f4eded798741c51300a030a765c815c
OWendt <dielanie@freenet.de>
2020-05-08 10:33:10 +0200

    small refactoring - add http for express server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
 
-const port = process.env.PORT
-
 app = express()
+const server = http.createServer(app)
 
-// Define paths for Express config
+const port = process.env.PORT
 const publicDirectoryPath = path.join(__dirname,'../public')
-
-// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server is up and running on Port', port)
 })
 
