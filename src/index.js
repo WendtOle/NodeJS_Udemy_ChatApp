48ec7d39aebc829ca6afbd686a1b1462e5f126c0
OWendt <dielanie@freenet.de>
2020-05-09 10:44:44 +0200

    message when user joined or left the chat

--- src/index.js
+++ src/index.js
@@ -16,12 +16,17 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message','A new user has joined!')
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
     })
 
+    socket.on('disconnect', () => {
+        io.emit('message','A user has left!')
+    })
 })
+
 server.listen(port, () => {
     console.log('Server is up and running on Port', port)
 })
